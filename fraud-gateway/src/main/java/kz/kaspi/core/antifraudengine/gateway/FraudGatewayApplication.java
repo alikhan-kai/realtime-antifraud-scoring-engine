24f0578f9238ac532ae3be17074824ee154ada8a
Alikhan-kai <alikhannurlanov.280207@gmail.com>
2026-09-21 16:08:59 +0500

    Инициализация мультимодульного проекта Anti-Fraud Engine (Gateway & Stream Analytics) и базовой инфраструктуры

---
+++ fraud-gateway/src/main/java/kz/kaspi/core/antifraudengine/gateway/FraudGatewayApplication.java
@@ -0,0 +1,13 @@
+package kz.kaspi.core.antifraudengine.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudGatewayApplication.class, args);
+	}
+
+}
---
+++ stream-analytics/src/main/java/kz/kaspi/core/antifraudengine/analytics/StreamAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package kz.kaspi.core.antifraudengine.analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamAnalyticsApplication.class, args);
+	}
+
+}
