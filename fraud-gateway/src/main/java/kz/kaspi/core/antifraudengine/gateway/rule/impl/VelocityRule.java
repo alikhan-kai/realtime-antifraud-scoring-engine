5d88462eeb07bf8944dd43c9fcbc2701e2551446
Alikhan-kai <alikhannurlanov.280207@gmail.com>
2026-09-22 23:34:37 +0500

    feat(gateway): Добавлен каркас для правила проверки частоты транзакций (VelocityRule)

---
+++ fraud-gateway/src/main/java/kz/kaspi/core/antifraudengine/gateway/rule/impl/VelocityRule.java
@@ -0,0 +1,21 @@
+package kz.kaspi.core.antifraudengine.gateway.rule.impl;
+
+import kz.kaspi.core.antifraudengine.gateway.domain.RuleResult;
+import kz.kaspi.core.antifraudengine.gateway.domain.TransactionEvent;
+import kz.kaspi.core.antifraudengine.gateway.rule.FraudRule;
+import org.springframework.stereotype.Component;
+
+@Component
+public class VelocityRule implements FraudRule {
+
+    @Override
+    public String ruleName() {
+        return "VELOCITY_SURGE_RULE";
+    }
+
+    @Override
+    public RuleResult evaluate(TransactionEvent event) {
+        // TODO: Реализовать обращение к Kafka Streams для подсчета транзакций за 60 секунд.
+        return new RuleResult(ruleName(), 0, "Проверка частоты временно отключена");
+    }
+}
